806d1a5893b84b483365512d6eee1f3d2ac355d2
niderman <viktor.niderman@gmail.com>
2024-10-03 21:32:04 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import './assets/main.css'
+import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
+
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,88 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+
+const text1 = ref('');
+const text2 = ref('');
+const text3 = ref('');
+
+const save = () => {
+  const data = JSON.stringify({
+    text1: text1.value,
+    text2: text2.value,
+    text3: text3.value,
+  });
+  localStorage.setItem('store', data);
+  fetch('https://api.niderman.pro/api/planners', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ data })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error:' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(planner => {
+    console.log('Clan created:', planner);
+  })
+  .catch(error => {
+    console.log(error)
+    alert('Error');
+  });
+}
+
+const load = () => {
+  const data = localStorage.getItem('store');
+  if (data) {
+    const parsed = JSON.parse(data);
+    text1.value = parsed.text1;
+    text2.value = parsed.text2;
+    text3.value = parsed.text3;
+  }
+
+
+  fetch('https://api.niderman.pro/api/planners/latest')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Not found');
+    }
+    return response.json();
+  })
+  .then(planner => {
+    const lastPlan = JSON.parse(planner.data);
+    text1.value = lastPlan.text1;
+    text2.value = lastPlan.text2;
+    text3.value = lastPlan.text3;
+    console.log('Last plan:', planner);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+onMounted(() => {
+  load();
+})
 
 </script>
 
 <template>
-  <main>
-    Home
+  <main class="d-flex pa-2">
+    <div class="flex-grow-1 pa-1">
+      <h3>Current Tasks</h3>
+      <v-textarea v-model="text1" @input="save" label="Label" rows="40"></v-textarea>
+    </div>
+    <div class="flex-grow-1 pa-1">
+      <h3>Future Tasks</h3>
+      <v-textarea v-model="text2" @input="save" label="Label" rows="40"></v-textarea>
+    </div>
+    <div class="flex-grow-1 pa-1">
+      <h3>Buy list</h3>
+      <v-textarea v-model="text3" @input="save" label="Label" rows="40"></v-textarea>
+    </div>
+
   </main>
 </template>
