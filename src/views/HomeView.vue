626ff60cc7cc73c500826fd3313a6fa8c380b8b7
niderman <viktor.niderman@gmail.com>
2024-10-06 15:27:37 +0100

    save to backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,24 +2,24 @@
 import { onMounted, ref } from 'vue'
 import { loadFromBackend, saveToBackend } from '@/helpers/backend.js'
 
-const text1 = ref('');
-const text2 = ref('');
-const text3 = ref('');
+const currentTasks = ref('');
+const futureTasks = ref('');
+const toBuyList = ref('');
 
 const save = () => {
   const data = JSON.stringify({
-    text1: text1.value,
-    text2: text2.value,
-    text3: text3.value,
+    currentTasks: currentTasks.value,
+    futureTasks: futureTasks.value,
+    toBuyList: toBuyList.value,
   });
   saveToBackend(data);
 }
 
 const load = () => {
   loadFromBackend().then(data => {
-    text1.value = data.text1;
-    text2.value = data.text2;
-    text3.value = data.text3;
+    currentTasks.value = data.currentTasks;
+    futureTasks.value = data.futureTasks;
+    toBuyList.value = data.toBuyList;
   })
 }
 
@@ -33,15 +33,15 @@ onMounted(() => {
   <main class="d-flex pa-2">
     <div class="flex-grow-1 pa-1">
       <h3>Current Tasks</h3>
-      <v-textarea v-model="text1" @input="save" label="Label" rows="40"></v-textarea>
+      <v-textarea v-model="currentTasks" @input="save" label="Label" rows="40"></v-textarea>
     </div>
     <div class="flex-grow-1 pa-1">
       <h3>Future Tasks</h3>
-      <v-textarea v-model="text2" @input="save" label="Label" rows="40"></v-textarea>
+      <v-textarea v-model="futureTasks" @input="save" label="Label" rows="40"></v-textarea>
     </div>
     <div class="flex-grow-1 pa-1">
       <h3>Buy list</h3>
-      <v-textarea v-model="text3" @input="save" label="Label" rows="40"></v-textarea>
+      <v-textarea v-model="toBuyList" @input="save" label="Label" rows="40"></v-textarea>
     </div>
 
   </main>
