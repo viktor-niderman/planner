c872e850344b05763024a8e766e8f3f8ff66fc8d
niderman <viktor.niderman@gmail.com>
2024-10-11 13:19:50 +0100

    PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,44 @@ import { defineConfig } from 'vite';
 import { config } from 'dotenv';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
+import { VitePWA } from 'vite-plugin-pwa'
 
 config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react(), wasm(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Notes',
+        short_name: 'Notes',
+        description: 'My own notes',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+
+      }
+    })],
   optimizeDeps: {
     // Убедитесь, что Automerge не исключён из оптимизации
     include: ['@automerge/automerge']
