59fea9777a45ed6576a83e35a545f997ec36dba5
niderman <viktor.niderman@gmail.com>
2024-10-24 00:07:59 +0100

    dark theme

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,31 +1,58 @@
 import React from 'react'
 import {
-  Box,
+  Box, Button, Menu, MenuItem,
 } from '@mui/material'
 import useUserStore from '../store/userStore.js'
 import ThemeSwitcher from './ThemeSwitcher.jsx'
 
 function Header (props) {
   const user = useUserStore()
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box className="header" sx={{
       display: 'flex',
       justifyContent: 'space-between',
       padding: '0 5px 0',
     }}>
-      <div className="import-export-section">
-        <button onClick={props.exportCallback}
-                className="export-button">Export JSON
-        </button>
-        <input type="file" accept=".json"
-               onChange={props.importCallback}
-               className="import-input"/>
-      </div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={props.exportCallback}>Export</MenuItem>
+        <MenuItem>
+          <input type="file" accept=".json"
+                 onChange={props.importCallback}
+                 className="import-input"/>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          {user.name}
+        </MenuItem>
+      </Menu>
       <div>
         <ThemeSwitcher/>
-      </div>
-      <div>
-        {user.name}
       </div>
     </Box>
   )
